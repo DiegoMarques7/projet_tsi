ecfaa00a8da2d022da3f14dc72e24a17fd928a70
DiegoMarques7 <72151757+DiegoMarques7@users.noreply.github.com>
2022-01-08 20:01:52 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,522 @@
+/*****************************************************************************\
+ * TP CPE, 4ETI, TP synthese d'images
+ * --------------
+ *
+ * Programme principal des appels OpenGL
+ \*****************************************************************************/
+
+#include "declaration.h"
+#include <stdio.h>
+#include <stdlib.h>
+//identifiant des shaders
+GLuint shader_program_id;
+GLuint gui_program_id;
+
+camera cam;
+
+const int nb_obj = 6;
+objet3d obj[nb_obj];
+
+const int nb_text = 5;
+text text_to_draw[nb_text];
+
+int temps = 0;
+int score = 0;
+
+bool perdu = false;
+/*****************************************************************************\
+* initialisation                                                              *
+\*****************************************************************************/
+static void init()
+{
+  shader_program_id = glhelper::create_program_from_file("shaders/shader.vert", "shaders/shader.frag"); CHECK_GL_ERROR();
+
+  cam.projection = matrice_projection(60.0f*M_PI/180.0f,1.0f,0.01f,100.0f);
+  cam.tr.translation = vec3(0.0f, 2.0f, 0.0f);
+  cam.tr.translation = vec3(0.0f, 40.0f, 0.0f);
+  cam.tr.rotation_center = vec3(0.0f, 40.0f, 0.0f);
+  cam.tr.rotation_euler = vec3(M_PI/2., 0.0f, 0.0f);
+
+  init_model_1();
+  init_model_2();
+  init_model_3();
+
+  gui_program_id = glhelper::create_program_from_file("shaders/gui.vert", "shaders/gui.frag"); CHECK_GL_ERROR();
+
+  text_to_draw[0].value = "Timer";
+  text_to_draw[0].bottomLeft = vec2(0.2, 0.92);
+  text_to_draw[0].topRight = vec2(0.7, 1.3);
+  init_text(text_to_draw);
+
+  
+
+  text_to_draw[1]=text_to_draw[0];
+  text_to_draw[1].value = "Score";
+  text_to_draw[1].bottomLeft.y = 0.80f;
+  text_to_draw[1].topRight.y = 1.2f;
+}
+
+/*****************************************************************************\
+* display_callback                                                           *
+\*****************************************************************************/
+ static void display_callback()
+{
+  glClearColor(0.5f, 0.6f, 0.9f, 1.0f); CHECK_GL_ERROR();
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); CHECK_GL_ERROR();
+
+  for(int i = 0; i < nb_obj; ++i)
+    draw_obj3d(obj + i, cam);
+
+  for(int i = 0; i < nb_text; ++i)
+    draw_text(text_to_draw + i);
+
+  glutSwapBuffers();
+}
+
+
+//Transformation des modeles
+transformation tr;
+
+/*****************************************************************************\
+* keyboard_callback                                                           *
+\*****************************************************************************/
+static void keyboard_callback(unsigned char key, int, int)
+{
+  float d_angle = 1.0f;
+  switch (key)
+  {
+    case 'p':
+      glhelper::print_screen();
+      break;
+    case 'q':
+    case 'Q':
+    case 27:
+      exit(0);
+      break;
+
+    case 'o':
+      cam.tr.translation.y += d_angle;
+      break;
+    case 'l':
+      cam.tr.translation.y -= d_angle;
+      break;
+
+    case 'k':
+      cam.tr.translation.x -= d_angle;
+      break;
+    case 'm':
+      cam.tr.translation.x += d_angle;
+      break;
+
+  }
+}
+
+/*****************************************************************************\
+* special_callback                                                            *
+\*****************************************************************************/
+static void special_callback(int key, int,int)
+{
+//to do programme_11
+  float dL=1.5f;
+  switch (key)
+  {
+    case GLUT_KEY_UP:
+      if (obj[2].tr.translation.z > -20){
+      obj[2].tr.translation.z -= dL; //rotation avec la touche du haut
+      }
+      break;
+    case GLUT_KEY_DOWN:
+      obj[2].tr.translation.z += dL; //rotation avec la touche du bas
+      break;
+    case GLUT_KEY_LEFT:
+      if (obj[2].tr.translation.x > -20){
+      obj[2].tr.translation.x -= dL; //rotation avec la touche de gauche
+      }
+      break;
+    case GLUT_KEY_RIGHT:
+      if (obj[2].tr.translation.x < 20){
+      obj[2].tr.translation.x += dL; //rotation avec la touche de droite
+      }
+      break;
+  }
+}
+
+float a =(double) rand()/RAND_MAX;
+float b =(double) rand()/RAND_MAX;
+float c =(double) rand()/RAND_MAX;
+float d =(double) rand()/RAND_MAX;
+
+/*****************************************************************************\
+* timer_callback                                                              *
+\*****************************************************************************/
+static void timer_callback(int)
+{
+  obj[0].tr.translation.x += (a+0.25);
+  if (obj[0].tr.translation.x >30.0f){
+    obj[0].tr.translation.x -= 55.0f;
+  }
+
+  obj[3].tr.translation.x -= (b+0.25);
+  if (obj[3].tr.translation.x <-30.0f){
+    obj[3].tr.translation.x += 50.0f;
+  }
+
+  obj[5].tr.translation.x += (c+0.25);
+  if (obj[5].tr.translation.x >30.0f){
+    obj[5].tr.translation.x -= 55.0f;
+  }
+
+  obj[4].tr.translation.x -= (d+0.25);
+  if (obj[4].tr.translation.x <-30.0f){
+    obj[4].tr.translation.x += 50.0f;
+  }
+
+  collision();
+  win();
+  glutTimerFunc(25, timer_callback, 0);
+  glutPostRedisplay();
+  
+}
+
+
+static void compteur(int)
+{ if (perdu == false){
+  temps += 1;
+  text_to_draw[2]=text_to_draw[0];
+  text_to_draw[2].value = std::to_string(temps);
+  text_to_draw[2].bottomLeft = vec2(0.7, 0.9);
+  text_to_draw[2].topRight = vec2(1, 0.99);
+
+  text_to_draw[4]=text_to_draw[0];
+  text_to_draw[4].value = std::to_string(score); 
+  text_to_draw[4].bottomLeft = vec2(0.65, 0.8);
+  text_to_draw[4].topRight = vec2(0.95, 0.89);
+  
+  glutTimerFunc(1000, compteur, 0);
+  glutPostRedisplay();
+}
+}
+
+void collision(){
+int NumeroDinosaure[4]={1,3,4,5};
+for (int i=0; i<4; i++){
+ if (abs(obj[2].tr.translation.x -obj[NumeroDinosaure[i]].tr.translation.x)<5 && (abs(obj[2].tr.translation.z -obj[NumeroDinosaure[i]].tr.translation.z)<2) ){
+  obj[1].texture_id = glhelper::load_texture("data/natani.tga");
+  obj[0].visible=false;
+  obj[3].visible=false;
+  obj[2].visible=false;
+  obj[4].visible=false;
+  obj[5].visible=false;
+  text_to_draw[3]=text_to_draw[0];
+  text_to_draw[3].value = "T'es mauvais Jack"; 
+  text_to_draw[3].bottomLeft = vec2(-0.9, -0.9);
+  text_to_draw[3].topRight = vec2(0.9, 0.9);
+  perdu = true;
+}
+
+}
+}
+
+void win (){
+  if (obj[2].tr.translation.z > 22.0f) {
+    obj[2].tr.translation.z = -15.0f;
+    score += 1;
+}
+
+}
+/*****************************************************************************\
+* main                                                                         *
+\*****************************************************************************/
+int main(int argc, char** argv)
+{
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH | MACOSX_COMPATIBILITY);
+  glutInitWindowSize(600, 600);
+  glutCreateWindow("OpenGL");
+
+  glutDisplayFunc(display_callback);
+  glutKeyboardFunc(keyboard_callback);
+  glutSpecialFunc(special_callback);
+  glutTimerFunc(25, timer_callback, 0);
+  glutTimerFunc(1000, compteur, 0);
+
+  glewExperimental = true;
+  glewInit();
+
+  std::cout << "OpenGL: " << (GLchar *)(glGetString(GL_VERSION)) << std::endl;
+
+  init();
+  glutMainLoop();
+
+  return 0;
+}
+
+/*****************************************************************************\
+* draw_text                                                                   *
+\*****************************************************************************/
+void draw_text(const text * const t)
+{
+  if(!t->visible) return;
+  
+  glDisable(GL_DEPTH_TEST);
+  glUseProgram(t->prog);
+
+  vec2 size = (t->topRight - t->bottomLeft) / float(t->value.size());
+  
+  GLint loc_size = glGetUniformLocation(gui_program_id, "size"); CHECK_GL_ERROR();
+  if (loc_size == -1) std::cerr << "Pas de variable uniforme : size" << std::endl;
+  glUniform2f(loc_size,size.x, size.y);     CHECK_GL_ERROR();
+
+  glBindVertexArray(t->vao);                CHECK_GL_ERROR();
+  
+  for(unsigned i = 0; i < t->value.size(); ++i)
+  {
+    GLint loc_start = glGetUniformLocation(gui_program_id, "start"); CHECK_GL_ERROR();
+    if (loc_start == -1) std::cerr << "Pas de variable uniforme : start" << std::endl;
+    glUniform2f(loc_start,t->bottomLeft.x+i*size.x, t->bottomLeft.y);    CHECK_GL_ERROR();
+
+    GLint loc_char = glGetUniformLocation(gui_program_id, "c"); CHECK_GL_ERROR();
+    if (loc_char == -1) std::cerr << "Pas de variable uniforme : c" << std::endl;
+    glUniform1i(loc_char, (int)t->value[i]);    CHECK_GL_ERROR();
+    glBindTexture(GL_TEXTURE_2D, t->texture_id);                            CHECK_GL_ERROR();
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);                    CHECK_GL_ERROR();
+  }
+}
+
+/*****************************************************************************\
+* draw_obj3d                                                                  *
+\*****************************************************************************/
+void draw_obj3d(const objet3d* const obj, camera cam)
+{
+  if(!obj->visible) return;
+
+  glEnable(GL_DEPTH_TEST);
+  glUseProgram(obj->prog);
+  
+  {
+    GLint loc_projection = glGetUniformLocation(shader_program_id, "projection"); CHECK_GL_ERROR();
+    if (loc_projection == -1) std::cerr << "Pas de variable uniforme : projection" << std::endl;
+    glUniformMatrix4fv(loc_projection,1,false,pointeur(cam.projection));    CHECK_GL_ERROR();
+
+    GLint loc_rotation_view = glGetUniformLocation(shader_program_id, "rotation_view"); CHECK_GL_ERROR();
+    if (loc_rotation_view == -1) std::cerr << "Pas de variable uniforme : rotation_view" << std::endl;
+    mat4 rotation_x = matrice_rotation(cam.tr.rotation_euler.x, 1.0f, 0.0f, 0.0f);
+    mat4 rotation_y = matrice_rotation(cam.tr.rotation_euler.y, 0.0f, 1.0f, 0.0f);
+    mat4 rotation_z = matrice_rotation(cam.tr.rotation_euler.z, 0.0f, 0.0f, 1.0f);
+    glUniformMatrix4fv(loc_rotation_view,1,false,pointeur(rotation_x*rotation_y*rotation_z));    CHECK_GL_ERROR();
+
+    vec3 cv = cam.tr.rotation_center;
+    GLint loc_rotation_center_view = glGetUniformLocation(shader_program_id, "rotation_center_view"); CHECK_GL_ERROR();
+    if (loc_rotation_center_view == -1) std::cerr << "Pas de variable uniforme : rotation_center_view" << std::endl;
+    glUniform4f(loc_rotation_center_view , cv.x,cv.y,cv.z , 0.0f); CHECK_GL_ERROR();
+
+    vec3 tv = cam.tr.translation;
+    GLint loc_translation_view = glGetUniformLocation(shader_program_id, "translation_view"); CHECK_GL_ERROR();
+    if (loc_translation_view == -1) std::cerr << "Pas de variable uniforme : translation_view" << std::endl;
+    glUniform4f(loc_translation_view , tv.x,tv.y,tv.z , 0.0f); CHECK_GL_ERROR();
+  }
+  {
+    GLint loc_rotation_model = glGetUniformLocation(obj->prog, "rotation_model"); CHECK_GL_ERROR();
+    if (loc_rotation_model == -1) std::cerr << "Pas de variable uniforme : rotation_model" << std::endl;
+    mat4 rotation_x = matrice_rotation(obj->tr.rotation_euler.x, 1.0f, 0.0f, 0.0f);
+    mat4 rotation_y = matrice_rotation(obj->tr.rotation_euler.y, 0.0f, 1.0f, 0.0f);
+    mat4 rotation_z = matrice_rotation(obj->tr.rotation_euler.z, 0.0f, 0.0f, 1.0f);
+    glUniformMatrix4fv(loc_rotation_model,1,false,pointeur(rotation_x*rotation_y*rotation_z));    CHECK_GL_ERROR();
+
+    vec3 c = obj->tr.rotation_center;
+    GLint loc_rotation_center_model = glGetUniformLocation(obj->prog, "rotation_center_model");   CHECK_GL_ERROR();
+    if (loc_rotation_center_model == -1) std::cerr << "Pas de variable uniforme : rotation_center_model" << std::endl;
+    glUniform4f(loc_rotation_center_model , c.x,c.y,c.z , 0.0f);                                  CHECK_GL_ERROR();
+
+    vec3 t = obj->tr.translation;
+    GLint loc_translation_model = glGetUniformLocation(obj->prog, "translation_model"); CHECK_GL_ERROR();
+    if (loc_translation_model == -1) std::cerr << "Pas de variable uniforme : translation_model" << std::endl;
+    glUniform4f(loc_translation_model , t.x,t.y,t.z , 0.0f);                                     CHECK_GL_ERROR();
+  }
+  glBindVertexArray(obj->vao);                                              CHECK_GL_ERROR();
+
+  glBindTexture(GL_TEXTURE_2D, obj->texture_id);                            CHECK_GL_ERROR();
+  glDrawElements(GL_TRIANGLES, 3*obj->nb_triangle, GL_UNSIGNED_INT, 0);     CHECK_GL_ERROR();
+}
+
+void init_text(text *t){
+  vec3 p0=vec3( 0.0f, 0.0f, 0.0f);
+  vec3 p1=vec3( 0.0f, 1.0f, 0.0f);
+  vec3 p2=vec3( 1.0f, 1.0f, 0.0f);
+  vec3 p3=vec3( 1.0f, 0.0f, 0.0f);
+
+  vec3 geometrie[4] = {p0, p1, p2, p3}; 
+  triangle_index index[2] = { triangle_index(0, 1, 2), triangle_index(0, 2, 3)};
+
+  glGenVertexArrays(1, &(t->vao));                                              CHECK_GL_ERROR();
+  glBindVertexArray(t->vao);                                                  CHECK_GL_ERROR();
+
+  GLuint vbo;
+  glGenBuffers(1, &vbo);                                                       CHECK_GL_ERROR();
+  glBindBuffer(GL_ARRAY_BUFFER,vbo);                                          CHECK_GL_ERROR();
+  glBufferData(GL_ARRAY_BUFFER,sizeof(geometrie),geometrie,GL_STATIC_DRAW);   CHECK_GL_ERROR();
+
+  glEnableVertexAttribArray(0); CHECK_GL_ERROR();
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0); CHECK_GL_ERROR();
+
+  GLuint vboi;
+  glGenBuffers(1,&vboi);                                                      CHECK_GL_ERROR();
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,vboi);                                 CHECK_GL_ERROR();
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(index),index,GL_STATIC_DRAW);   CHECK_GL_ERROR();
+
+  t->texture_id = glhelper::load_texture("data/fontB.tga");
+
+  t->visible = true;
+  t->prog = gui_program_id;
+}
+
+GLuint upload_mesh_to_gpu(const mesh& m)
+{
+  GLuint vao, vbo, vboi;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+
+  glGenBuffers(1,&vbo);                                 CHECK_GL_ERROR();
+  glBindBuffer(GL_ARRAY_BUFFER,vbo); CHECK_GL_ERROR();
+  glBufferData(GL_ARRAY_BUFFER,m.vertex.size()*sizeof(vertex_opengl),&m.vertex[0],GL_STATIC_DRAW); CHECK_GL_ERROR();
+
+  glEnableVertexAttribArray(0); CHECK_GL_ERROR();
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertex_opengl), 0); CHECK_GL_ERROR();
+
+  glEnableVertexAttribArray(1); CHECK_GL_ERROR();
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_TRUE, sizeof(vertex_opengl), (void*)sizeof(vec3)); CHECK_GL_ERROR();
+
+  glEnableVertexAttribArray(2); CHECK_GL_ERROR();
+  glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(vertex_opengl), (void*)(2*sizeof(vec3))); CHECK_GL_ERROR();
+
+  glEnableVertexAttribArray(3); CHECK_GL_ERROR();
+  glVertexAttribPointer(3, 2, GL_FLOAT, GL_FALSE, sizeof(vertex_opengl), (void*)(3*sizeof(vec3))); CHECK_GL_ERROR();
+
+  glGenBuffers(1,&vboi); CHECK_GL_ERROR();
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,vboi); CHECK_GL_ERROR();
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER,m.connectivity.size()*sizeof(triangle_index),&m.connectivity[0],GL_STATIC_DRAW); CHECK_GL_ERROR();
+
+  return vao;
+}
+
+void init_model_1()
+{
+  // Chargement d'un maillage a partir d'un fichier
+  mesh m = load_obj_file("data/stegosaurus.obj");
+
+  // Affecte une transformation sur les sommets du maillage
+  float s = 1.2f;
+  mat4 transform = mat4(   s, 0.0f, 0.0f, 0.0f,
+      0.0f,    s, 0.0f, 0.0f,
+      0.0f, 0.0f,   s , 0.0f,
+      0.0f, 0.0f, 0.0f, 1.0f);
+  apply_deformation(&m,transform);
+
+  // Centre la rotation du modele 1 autour de son centre de gravite approximatif
+  obj[0].tr.rotation_center = vec3(0.0f,0.0f,0.0f);
+  obj[0].tr.rotation_euler = vec3(0.0f,1.6f,0.0f);
+
+  update_normals(&m);
+  fill_color(&m,vec3(1.0f,1.0f,1.0f));
+
+  obj[0].vao = upload_mesh_to_gpu(m);
+
+  obj[0].nb_triangle = m.connectivity.size();
+  obj[0].texture_id = glhelper::load_texture("data/stegosaurus.tga");
+  obj[0].visible = true;
+  obj[0].prog = shader_program_id;
+
+  obj[0].tr.translation = vec3(-15.0, 0.0, 10.0);
+
+  obj[3]=obj[0];
+  obj[3].tr.translation = vec3(30.0, 0.0, 1.0);
+  obj[3].tr.rotation_euler = vec3(0.0f,-1.6f,0.0f);
+
+  obj[4]=obj[0];
+  obj[4].tr.translation = vec3(30.0, 0.0, -5.0);
+  obj[4].tr.rotation_euler = vec3(0.0f,-1.6f,0.0f);
+
+  obj[5]=obj[0];
+  obj[5].tr.translation = vec3(-30.0, 0.0, 17.0);
+  
+}
+
+void init_model_2()
+{
+
+  mesh m;
+
+  //coordonnees geometriques des sommets
+  vec3 p0=vec3(-25.0f,0.0f,-25.0f);
+  vec3 p1=vec3( 25.0f,0.0f,-25.0f);
+  vec3 p2=vec3( 25.0f,0.0f, 25.0f);
+  vec3 p3=vec3(-25.0f,0.0f, 25.0f);
+
+  //normales pour chaque sommet
+  vec3 n0=vec3(0.0f,1.0f,0.0f);
+  vec3 n1=n0;
+  vec3 n2=n0;
+  vec3 n3=n0;
+
+  //couleur pour chaque sommet
+  vec3 c0=vec3(1.0f,1.0f,1.0f);
+  vec3 c1=c0;
+  vec3 c2=c0;
+  vec3 c3=c0;
+
+  //texture du sommet
+  vec2 t0=vec2(0.0f,0.0f);
+  vec2 t1=vec2(1.0f,0.0f);
+  vec2 t2=vec2(1.0f,1.0f);
+  vec2 t3=vec2(0.0f,1.0f);
+
+  vertex_opengl v0=vertex_opengl(p0,n0,c0,t0);
+  vertex_opengl v1=vertex_opengl(p1,n1,c1,t1);
+  vertex_opengl v2=vertex_opengl(p2,n2,c2,t2);
+  vertex_opengl v3=vertex_opengl(p3,n3,c3,t3);
+
+  m.vertex = {v0, v1, v2, v3};
+
+  //indice des triangles
+  triangle_index tri0=triangle_index(0,1,2);
+  triangle_index tri1=triangle_index(0,2,3);  
+  m.connectivity = {tri0, tri1};
+
+  obj[1].nb_triangle = 2;
+  obj[1].vao = upload_mesh_to_gpu(m);
+
+  obj[1].texture_id = glhelper::load_texture("data/route1.tga");
+
+  obj[1].visible = true;
+  obj[1].prog = shader_program_id;
+}
+
+
+void init_model_3()
+{
+  // Chargement d'un maillage a partir d'un fichier
+  mesh m = load_obj_file("data/stickman.OBJ");
+
+  //Affecte une transformation sur les sommets du maillage
+  float s = 0.1f;
+  mat4 transform = mat4(   s, 0.0f, 0.0f, 0.0f,
+      0.0f,    s, 0.0f, 0.50f,
+      0.0f, 0.0f,   s , 0.0f,
+      0.0f, 0.0f, 0.0f, 1.0f);
+  //apply_deformation(&m,matrice_rotation(M_PI/2.0f,1.0f,0.0f,0.0f));
+  //apply_deformation(&m,matrice_rotation(M_PI,0.0f,1.0f,0.0f));
+  apply_deformation(&m,transform);
+
+  update_normals(&m);
+  fill_color(&m,vec3(1.0f,1.0f,1.0f));
+
+  obj[2].vao = upload_mesh_to_gpu(m);
+
+  obj[2].nb_triangle = m.connectivity.size();
+  obj[2].texture_id = glhelper::load_texture("data/white.tga");
+
+  obj[2].visible = true;
+  obj[2].prog = shader_program_id;
+
+  obj[2].tr.translation = vec3(0.0, 0.0, -15.0);
+}
